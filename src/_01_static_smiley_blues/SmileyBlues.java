07459c936f4864b548a9c08e2c472b656a04d54f
emmiekao <emmiekao@localhost>
2021-03-12 18:29:58 -0800

    robot jar

--- src/_01_static_smiley_blues/SmileyBlues.java
+++ src/_01_static_smiley_blues/SmileyBlues.java
@@ -7,8 +7,9 @@ public class SmileyBlues {
 	void start() {
 		// 1. Make a new JFrame and set it to be visible
 JFrame frame=new JFrame();
+frame.setVisible(true);
 		// 2. Set your frame's default close operation to JFrame.EXIT_ON_CLOSE
-JFrame.EXIT_ON_CLOSE
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 3. Make a new JPanel
 JPanel panel=new JPanel();
 		// 4. Add your panel to your frame
@@ -17,8 +18,15 @@ frame.add(panel);
 		// Set the Smiley class's color variable to Smiley.YELLOW (NOT a specific smiley object's!)
 		// Using a specific object for a static variable works, but is not usually recommended.
 		// Run it again; are your Smileys all yellow now? Also try changing them to red or green!
+Smiley.color=Smiley.GREEN;
 
 		// 5. Make three Smiley objects and add them to your panel
+Smiley smiley1=new Smiley();
+Smiley smiley2=new Smiley();
+Smiley smiley3=new Smiley();
+panel.add(smiley1);
+panel.add(smiley2);
+panel.add(smiley3);
 
 		// 6. Pack your frame
 frame.pack();
--- src/_02_backpack/Backpack.java
+++ src/_02_backpack/Backpack.java
@@ -13,8 +13,17 @@ public class Backpack {
 		// Your mission is to go to school.
 		// 1. First you need to put all your supplies into your backpack - use
 		// the putInBackpack(...) methods
-
+		
+		Backpack backpack=new Backpack();
+		pencil=new Pencil();
+		backpack.putInBackpack(pencil);
+		ruler=new Ruler();
+		backpack.putInBackpack(ruler);
+		textbook=new Textbook();
+		backpack.putInBackpack(textbook);
 		goToSchool();
+	
+		
 	}
 
 
